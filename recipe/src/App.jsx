b390b77f1a9127d07a4cfb35eb3e6513c50e44bd
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2024-06-04 02:21:17 +0500

    Home

--- recipe/src/App.jsx
+++ recipe/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Home2 from "./Components/Home2";
 
 function App() {
 
   return (
     <>
-      <Navbar/>      
+      <Navbar/>    
+      <div className="flex justify-center align-center">
+      <Home/>
+      </div>
     </>
   )
 }
---
+++ recipe/src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+
+      <h1 className="text-5xl font-bold mb-4 ml-4">
+        Discover delicious{" "}
+        <span className="text-yellow-600">
+          recipes <br /> <br />{" "}
+        </span>{" "}
+        for your next meal!
+      </h1>
+
+      
+    </>
+  );
+}
